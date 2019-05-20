d9d86b6ee42bfd325e1f50db119941edaf36ab85
102660231 <48194706+102660231@users.noreply.github.com>
2019-05-20 19:27:12 +1000

    amend

--- term2/week1/todofuntion.js
+++ term2/week1/todofuntion.js
@@ -1,16 +1,16 @@
 
 
-var addButton =document.getElementById("add");
+addButton = document.getElementById("item");
 addButton.onclick = addButtonClick;
 
-var removeButton = document.getElementById("remove");
+var removeButton = document.getElementById("remove all");
 removeButton.onclick= removeButtonClick;
 
 var singleButton = document.getElementById("single remove");
 singleButton.onclick = singleButtonClick;
 
-var button = document.getElementById("ourButton");
-button.onclick = ourButtonClick;
+// var button = document.getElementById("ourButton");
+// button.onclick = ourButtonClick;
 
 
 // initial array to store list of items
@@ -19,7 +19,6 @@ list =[];
 
 function addButtonClick() {
 
- // alert("click me Person");display message 
 
   var inputValue = document.getElementById("item").value;
   //var inputValue = user input;
@@ -30,20 +29,29 @@ function addButtonClick() {
   {
     // add item 
      //Now construct a quick list element
-    var li = "<li>" + inputValue+ "</li>";
+    //Defining a listener for our button, specifically, an onclick handler
+document.getElementById("item").onclick = function()
+ {
+  //First things first, we need our text:
+  var inputValue = document.getElementById("item").value; //.value gets input values
 
-    //Now use appendChild and add it to the list!
-    document.getElementById("list").appendChild(li);
+  //Now construct a quick list element
+  var li = "<li>" + text + "</li>";
 
-    list.push(inputValue);
-    document.getElementById("add").value="";
+  //Now use appendChild and add it to the list!
+  document.getElementById("list").appendChild(li);
+ }
+
+
+
+   // list.push(inputValue);
+   // document.getElementById("add").value="";
   }
 
 
   console.log(list)
   //clear screen
-  var list = document.getElementById("list").lastElementChild.innerHTML;
-  document.getElementById("demo").innerHTML = list;
+  
 }
  // var itemToAdd = document.getElementById("itemList");
   //var stayitem  = itemToAdd.lastElementChild;
@@ -51,7 +59,6 @@ function addButtonClick() {
  
       
 
-}
 function validationCheck(checkInputValue)
 {
     // to check text entered not a empty
